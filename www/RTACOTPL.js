0a1ba818a8621d59a4efc05d3e200afb9257446b
Tushar-Kanvinde <tskanvinde@gmail.com>
2024-08-08 12:49:17 +0530

    Update RTACOTPL.js

--- www/RTACOTPL.js
+++ www/RTACOTPL.js
@@ -1,10 +1,10 @@
 var exec = require('cordova/exec');
 var RTACOTPL={
     checkOTPLPrinterPaper:function(fnSuccess, fnError){
-        exec(fnSuccess, fnError, "RTAC", "checkOTPLPrinterPaper");
+        exec(fnSuccess, fnError, "RTACOTPL", "checkOTPLPrinterPaper");
     },
     printOTPLImage:function(fnSuccess, fnError,imageString){
-        exec(fnSuccess, fnError, "RTAC", "printBitmap",[imageString]);
+        exec(fnSuccess, fnError, "RTACOTPL", "printBitmap",[imageString]);
     },
 }
 module.exports = RTACOTPL;
